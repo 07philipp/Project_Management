c9c457ee0bd0d724dde0d8560adad9432de89ac4
ReadRebbit <134855820+ReadRebbit@users.noreply.github.com>
2026-06-02 23:55:45 +0200

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ $host = "localhost";
 $user = "root";
 $password = "";
 $database = "PM_System";
-$high_admin_pw = "20-Philipp-07";
-$high_admin_name = "Philipp";
+$high_admin_pw = "";
+$high_admin_name = "";
 ?>
